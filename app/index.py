5e1fd57bc7e00dc93dcfe1d366d3d7c69aa668c4
Lee Smith <leesmith110@googlemail.com>
2015-06-26 09:57:45 +0100

    Added some caching to the front end for now, should use varnish going forward

--- app/index.py
+++ app/index.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from flask import render_template
 from performance import get_dashboard_stats
+from flask.ext.cache import Cache
 app = Flask(__name__)
 
+cache = Cache(app,config={'CACHE_TYPE': 'simple'})
+
 @app.route("/")
+@cache.cached(timeout=50)
 def stats():
   return render_template('index.html', stats=get_dashboard_stats())
  
+@app.route("/clearcache")
+def clear_cache():
+  with app.app_context():
+    cache.clear()
+  return 'Cleared Cache'
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
